d0c0733f8bf72b14e86f23fbedcd62767a6d6196
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-14 11:06:14 +0530

    added documents

---
+++ Labs/Day-1/Day-1/Day-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // <data type> * pointer_name
+    /*
+        
+    */
+    int a = 5;
+    int* ptr;
+   
+    ptr = &a;
+    std::cout << ptr<<"\n";
+    std::cout << a<< "\n";
+    std::cout << *ptr << "\n";
+}
+
