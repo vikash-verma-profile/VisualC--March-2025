fdd706378f4b5943599daeab01f0d99f5233efe3
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-19 11:22:52 +0530

    added com

--- Labs/ServerDLL/ServerDLL/MyCOMObject.h
+++ Labs/ServerDLL/ServerDLL/MyCOMObject.h
@@ -43,13 +43,3 @@ public:
 	}
 };
 
-//Factory function to create the com object
-
-extern "C" __declspec(dllexport) HRESULT CreateInstance(REFCLSID clsid, REFIID iid, void** ppv) {
-	if (clsid == CLSID_MyCOMObject) {
-		MyComObject* obj = new MyComObject();
-		return obj->QueryInterface(iid, ppv);
-	}
-
-	return E_NOINTERFACE;
-}
--- Labs/ServerDLL/ServerDLL/MyComObject.cpp
+++ Labs/ServerDLL/ServerDLL/MyComObject.cpp
@@ -1,1 +1,13 @@
 #include "pch.h"
+#include "MyCOMObject.h"
+
+//Factory function to create the com object
+
+extern "C" __declspec(dllexport) HRESULT CreateInstance(REFCLSID clsid, REFIID iid, void** ppv) {
+	if (clsid == CLSID_MyCOMObject) {
+		MyComObject* obj = new MyComObject();
+		return obj->QueryInterface(iid, ppv);
+	}
+
+	return E_NOINTERFACE;
+}
