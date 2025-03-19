092057db04f38165f8b0df7492c4c9b2338049e4
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-19 10:39:26 +0530

    added com

--- Labs/ServerDLL/ServerDLL/IMyInterface.h
+++ Labs/ServerDLL/ServerDLL/IMyInterface.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <windows.h>
+#include <objbase.h>
+
+
+
+
+static const IID IID_IMyInterface = {
+	
+};
+__declspec(uuid("ddb76e28-5eb7-4d2b-bd9d-eeb1450edb7e"))
+class IMyInterface :public IUnknown {
+public:
+	virtual HRESULT STDMETHODCALLTYPE DoSomething() = 0;
+};
--- Labs/ServerDLL/ServerDLL/MyCOMObject.h
+++ Labs/ServerDLL/ServerDLL/MyCOMObject.h
@@ -1,1 +1,55 @@
 #pragma once
+#include "IMyInterface.h"
+#include <iostream>
+#include <objbase.h>
+
+using namespace std;
+static const IID CLSID_MyCOMObject = {
+
+};
+__declspec(uuid("8ff62f5b-bde0-4ffc-8a3a-b501023a2884"))
+class MyComObject :public IMyInterface {
+private:
+	ULONG refCount;
+public:
+	MyComObject() :refCount(1) {
+
+	}
+
+	HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid,void** ppvObject) override {
+		if (riid==IID_IUnknown || riid == IID_IMyInterface) {
+			*ppvObject = static_cast<IMyInterface*>(this);
+			AddRef();
+			return S_OK;
+		}
+		*ppvObject = nullptr;
+		return E_NOINTERFACE;
+	}
+
+	ULONG STDMETHODCALLTYPE AddRef() override {
+		return ++refCount;
+	}
+	ULONG STDMETHODCALLTYPE Release() override {
+		if (--refCount == 0) {
+			delete this;
+			return 0;
+		}
+		return refCount;
+	}
+
+	HRESULT STDMETHODCALLTYPE DoSomething() override {
+		cout << "Hello I am from COM Object" << endl;
+		return S_OK;
+	}
+};
+
+//Factory function to create the com object
+
+extern "C" __declspec(dllexport) HRESULT CreateInstance(REFCLSID clsid,REFIID iid,void** ppv) {
+	if (clsid == CLSID_MyCOMObject) {
+		MyComObject* obj = new MyComObject();
+		return obj->QueryInterface(iid,ppv);
+	}
+
+	return E_NOINTERFACE;
+}
