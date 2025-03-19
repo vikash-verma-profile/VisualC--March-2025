b5e012380a434744d7fcdc85072e7ab798e6d10b
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-19 11:04:12 +0530

    added com

--- Labs/ServerDLL/ServerDLL/MyCOMObject.h
+++ Labs/ServerDLL/ServerDLL/MyCOMObject.h
@@ -7,7 +7,7 @@ using namespace std;
 static const CLSID CLSID_MyCOMObject = {
 0x824c14dc, 0x692, 0x4671, {0xb7, 0xe7, 0xa4, 0xae, 0xdf, 0xf8, 0x20, 0x3b }
 };
-__declspec(uuid("8ff62f5b-bde0-4ffc-8a3a-b501023a2884"))
+//__declspec(uuid("8ff62f5b-bde0-4ffc-8a3a-b501023a2884"))
 class MyComObject :public IMyInterface {
 private:
 	ULONG refCount;
