04589e03f269456ca774205b60b80362b45dfb64
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-19 12:14:17 +0530

    added com and client

--- Labs/ServerDLLClient/ServerDLLClient/ServerDLLClient.cpp
+++ Labs/ServerDLLClient/ServerDLLClient/ServerDLLClient.cpp
@@ -1,20 +1,38 @@
-// ServerDLLClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
+#include "IMyInterface.h"
+
+using namespace std;
+
+typedef HRESULT(*CreateInstanceFunc)(REFCLSID, REFIID, void**);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	HMODULE hDLL = LoadLibrary(L"ServerDLL.dll");
+	if (!hDLL) {
+		cerr << "Failed to load COM server dll"<<endl;
+		return -1;
+	}
+
+	//get instance
+
+	CreateInstanceFunc CreateInstance = (CreateInstanceFunc)GetProcAddress(hDLL,"CreateInstance");
+	if (!CreateInstance) {
+		cerr << "Failed to get CreateInstance function" << endl;
+		FreeLibrary(hDLL);
+		return -1;
+	}
+	IMyInterface* pMyObj = nullptr;
+	HRESULT hr = CreateInstance(CLSID_MyCOMObject,IID_IMyInterface,(void**)&pMyObj);
+	if (SUCCEEDED(hr)) {
+		pMyObj->DoSomething();
+		pMyObj->Release();
+	}
+	else {
+		cerr << "Failed to create a com object" << endl;
+	}
+	FreeLibrary(hDLL);
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
