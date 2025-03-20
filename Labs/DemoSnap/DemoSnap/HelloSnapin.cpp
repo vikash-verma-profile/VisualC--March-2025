7f683243c0b580b167f8ff920a1692ff3aad6720
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-03-20 10:47:44 +0530

    added code

--- Labs/DemoSnap/DemoSnap/HelloSnapin.cpp
+++ Labs/DemoSnap/DemoSnap/HelloSnapin.cpp
@@ -13,8 +13,42 @@ STDMETHODIMP CHelloSnapin::CreateComponent(LPCOMPONENT* ppComponent) {
 	AddRef();
 	return S_OK;
 }
-STDMETHODIMP CHelloSnapin::DestroyComponent(void) {
-	Release();
-	return S_OK;
+
+STDMETHODIMP CHelloSnapin::Destroy(void) {
+    return S_OK;
+}
+
+STDMETHODIMP CHelloSnapin::GetDisplayInfo(SCOPEDATAITEM* pScopeDataItem) {
+    return E_NOTIMPL;
+}
+
+
+// IComponent Methods
+STDMETHODIMP CHelloSnapin::Initialize(LPCONSOLE lpConsole) {
+    return S_OK;
+}
+
+STDMETHODIMP CHelloSnapin::Notify(LPDATAOBJECT lpDataObject, MMC_NOTIFY_TYPE event, LPARAM arg, LPARAM param) {
+    return S_OK;
+}
+
+STDMETHODIMP CHelloSnapin::Destroy(MMC_COOKIE cookie) {
+    return S_OK;
+}
+
+STDMETHODIMP CHelloSnapin::GetResultViewType(MMC_COOKIE cookie, LPOLESTR* ppViewType, long* pViewOptions) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP CHelloSnapin::QueryDataObject(MMC_COOKIE cookie, DATA_OBJECT_TYPES type, LPDATAOBJECT* ppDataObject) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP CHelloSnapin::GetDisplayInfo(RESULTDATAITEM* pResultDataItem) {
+    return E_NOTIMPL;
+}
+
+STDMETHODIMP CHelloSnapin::CompareObjects(LPDATAOBJECT lpDataObjectA, LPDATAOBJECT lpDataObjectB) {
+    return E_NOTIMPL;
 }
 
--- Labs/DemoSnap/DemoSnap/HelloSnapin.h
+++ Labs/DemoSnap/DemoSnap/HelloSnapin.h
@@ -1,11 +1,7 @@
-// HelloSnapin.h : Declaration of the CHelloSnapin
-
 #pragma once
-#include "resource.h"       // main symbols
+#include "resource.h"     
 #include "DemoSnap_i.h"
 #include <mmc.h>
-
-
 
 #if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
 #error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
@@ -17,45 +13,44 @@ using namespace ATL;
 // CHelloSnapin
 
 class ATL_NO_VTABLE CHelloSnapin :
-	public CComObjectRootEx<CComSingleThreadModel>,
-	public CComCoClass<CHelloSnapin, &CLSID_HelloSnapin>,
-	/*public IDispatchImpl<IHelloSnapin, &IID_IHelloSnapin, &LIBID_DemoSnapLib,*/
-	public IComponentData,
-	public IComponent
+    public CComObjectRootEx<CComSingleThreadModel>,
+    public CComCoClass<CHelloSnapin, &CLSID_HelloSnapin>,
+    public IDispatchImpl<IHelloSnapin, &IID_IHelloSnapin, &LIBID_DemoSnapLib, 1, 0>,
+    public IComponentData,
+    public IComponent
 {
 public:
-	CHelloSnapin()
-	{
-	}
+    CHelloSnapin() {}
 
-	DECLARE_REGISTRY_RESOURCEID(106)
+    DECLARE_REGISTRY_RESOURCEID(106)
 
+    BEGIN_COM_MAP(CHelloSnapin)
+        COM_INTERFACE_ENTRY(IDispatch)
+        COM_INTERFACE_ENTRY(IHelloSnapin)
+        COM_INTERFACE_ENTRY(IComponentData)
+        COM_INTERFACE_ENTRY(IComponent)
+    END_COM_MAP()
 
-	BEGIN_COM_MAP(CHelloSnapin)
-		COM_INTERFACE_ENTRY(IComponentData)
-		COM_INTERFACE_ENTRY(IComponent)
-	END_COM_MAP()
+    // IComponentData methods
+    STDMETHOD(Initialize)(LPUNKNOWN pUnknown);
+    STDMETHOD(CreateComponent)(LPCOMPONENT* ppComponent);
+    STDMETHOD(Notify)(LPDATAOBJECT lpDataObject, MMC_NOTIFY_TYPE event, LPARAM arg, LPARAM param);
+    STDMETHOD(Destroy)(void);
+    STDMETHOD(QueryDataObject)(MMC_COOKIE cookie, DATA_OBJECT_TYPES type, LPDATAOBJECT* ppDataObject);
+    STDMETHOD(GetDisplayInfo)(SCOPEDATAITEM* pScopeDataItem);
+    STDMETHOD(CompareObjects)(LPDATAOBJECT lpDataObjectA, LPDATAOBJECT lpDataObjectB);
 
+    // IComponent methods
+    STDMETHOD(Initialize)(LPCONSOLE lpConsole);
+    STDMETHOD(Destroy)(MMC_COOKIE cookie);
+    STDMETHOD(GetResultViewType)(MMC_COOKIE cookie, LPOLESTR* ppViewType, long* pViewOptions);
+    STDMETHOD(GetDisplayInfo)(RESULTDATAITEM* pResultDataItem);
+   
+    DECLARE_PROTECT_FINAL_CONSTRUCT()
 
-	STDMETHOD(Initialize)(LPUNKNOWN pUnkown);
-	STDMETHOD(CreateComponent)(LPCOMPONENT* ppComponent);
-	STDMETHOD(DestroyComponent)(void);
-
-	DECLARE_PROTECT_FINAL_CONSTRUCT()
-
-	HRESULT FinalConstruct()
-	{
-		return S_OK;
-	}
-
-	void FinalRelease()
-	{
-	}
-
-public:
-
-
-
+    HRESULT FinalConstruct() { return S_OK; }
+    void FinalRelease() {}
 };
+
 
 OBJECT_ENTRY_AUTO(__uuidof(HelloSnapin), CHelloSnapin)
